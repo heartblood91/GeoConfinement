b002a9410620da6dfda88028ea2b4962671ee539
Cédric <github@hidemyhome.ovh>
2020-04-17 11:21:01 +0200

    mise en place des fichiers pour les actions redux

---
+++ actions/index.js
@@ -0,0 +1,5 @@
+import SET_PARAMS from "../actions/action-types";
+
+handleChangeParams = () => {
+  dispatch({ type: SET_PARAMS, payload: "qwerty" });
+};
--- reducers/params.js
+++ reducers/params.js
@@ -1,5 +1,18 @@
-export default function (state, action) {
-  return {
-    data: "azerty",
-  };
+import SET_PARAMS from "../actions/action-types";
+
+// initialise le store
+const initializeState = {
+  data: "azerty",
+};
+
+export default function (state = initializeState, action) {
+  switch (action.type) {
+    case SET_PARAMS:
+      return {
+        data: action.payload,
+      };
+
+    default:
+      return state;
+  }
 }
